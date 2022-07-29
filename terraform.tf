cca1b465c872a4b93c307dcdf7fd63288b14fcf2
Dgruploads <41875211+Dgruploads@users.noreply.github.com>
2022-07-29 21:18:06 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "ec2_example" {
   ami = "ami-12345"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "ec2_second_example" {
+  ami = "ami-67890"
+  instance_type = "t2.micro"
+}
